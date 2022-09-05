26b95f455c0d49f835b8ad942366be5eb1937340
ALEXECKERT93 <78118345+ALEXECKERT93@users.noreply.github.com>
2022-09-04 20:47:11 -0400

    Set Up project with log in screen

---
+++ app/src/main/java/com/example/gymbuddies/MainActivity.java
@@ -0,0 +1,39 @@
+package com.example.gymbuddies;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.gymbuddies.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+     //   NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+     //   NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+      //  NavigationUI.setupWithNavController(binding.navView, navController);
+
+    }
+
+
+}
