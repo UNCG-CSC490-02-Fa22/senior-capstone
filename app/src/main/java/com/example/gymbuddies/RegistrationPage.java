afb69846bb0c2bc3a3f27a49a4d75a182d6dede7
TyrellJ3 <tnjones5@uncg.edu>
2022-10-25 23:08:55 -0400

    trying to fix merge error

--- app/src/main/java/com/example/gymbuddies/RegistrationPage.java
+++ app/src/main/java/com/example/gymbuddies/RegistrationPage.java
@@ -1,44 +1,112 @@
 package com.example.gymbuddies;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import com.example.gymbuddies.databinding.RegistrationPageBinding;
+import java.util.HashMap;
+import java.util.Map;
 
+public class RegistrationPage extends AppCompatActivity {
+    private Button mRegister;
+    private EditText mEmail, mPassword, mName;
 
-public class RegistrationPage extends AppCompatActivity{
+    private RadioGroup mRadioGroup;
 
-        private RegistrationPageBinding binding;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registration_page);
 
-            binding = RegistrationPageBinding.inflate(getLayoutInflater());
-            setContentView(R.layout.registration_page);
-
-
-            Button button3;
-            Button button4;
-
-            button4 = (Button) findViewById(R.id.button4);
-            button3 = (Button) findViewById(R.id.button3);
-            button4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openNewActivity();
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user !=null){
+                    Intent intent = new Intent(RegistrationPage.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                    return;
                 }
-            });
-            button3.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    openNewActivity();
+            }
+        };
+
+
+        mRegister = (Button) findViewById(R.id.register);
+
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        mName = (EditText) findViewById(R.id.name);
+
+        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int selectId = mRadioGroup.getCheckedRadioButtonId();
+
+                final RadioButton radioButton = (RadioButton) findViewById(selectId);
+
+                if(radioButton.getText() == null){
+                    return;
                 }
-            });
+
+                final String email = mEmail.getText().toString();
+                final String password = mPassword.getText().toString();
+                final String name = mName.getText().toString();
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationPage.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(RegistrationPage.this, "sign up error", Toast.LENGTH_SHORT).show();
+                        }else{
+                            String userId = mAuth.getCurrentUser().getUid();
+                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
+                            Map userInfo = new HashMap<>();
+                            userInfo.put("name", name);
+                            userInfo.put("sex", radioButton.getText().toString());
+                            userInfo.put("profileImageUrl", "default");
+                            currentUserDb.updateChildren(userInfo);
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthStateListener);
+    }
+}
 
           //  BottomNavigationView navView = findViewById(R.id.nav_view);
             // Passing each menu ID as a set of Ids because each
@@ -49,15 +117,6 @@ public class RegistrationPage extends AppCompatActivity{
             //   NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
             //   NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
             //  NavigationUI.setupWithNavController(binding.navView, navController);
-        }
-        public void openNewActivity(){
-            Intent intent = new Intent(this, LoginOrRegisterActivity.class);
-            startActivity(intent);
-        }
-
-
-
-    }
 
 
 
