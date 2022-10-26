f4928a9403808b12cc4ad129cf3353ec96ac7a0c
bubblylotus <77747736+bubblylotus@users.noreply.github.com>
2022-10-26 01:06:42 -0400

    added bio card to view on click

--- app/src/main/java/com/example/gymbuddies/MatchBioActivity.java
+++ app/src/main/java/com/example/gymbuddies/MatchBioActivity.java
@@ -1,6 +1,7 @@
 package com.example.gymbuddies;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
 import android.view.View;
@@ -21,10 +22,17 @@ public class MatchBioActivity extends AppCompatActivity {
             //The key argument here must match that used in the other activity
         }
         TextView textView = (TextView) findViewById(R.id.editTextTextPersonName5);
-        textView.setText(value);
+        TextView skillLevel = (TextView) findViewById(R.id.skillLevel);
+        TextView bio = (TextView) findViewById(R.id.bio);
 
-        Button button = findViewById(R.id.bioButton);
-        button.setOnClickListener(new View.OnClickListener() {
+        String bioDesc = "I do not have a gym membership, I much prefer trails and hiking or playing rounds of disc golf, you know the lighter activities that doesn't always feel like working out, but more of being active. I am not opposed to finding a gym partner if that's your thing, but I have reached a plateau after losing 65 lbs in the last half year and I think having someone to be active with can help me turn the corner. Any ages or fitness levels are welcome as well as any gender. I work in Raleigh area and live in Sanford to give a better idea on a more local area.\n" +
+                "\n";
+        textView.setText(value);
+        bio.setText(bioDesc);
+        skillLevel.setText("Beginner");
+
+        CardView card = findViewById(R.id.cardView);
+        card.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
