55e0e7a22be5cfe7df497917226469ea2baec457
TyrellJ3 <tnjones5@uncg.edu>
2022-10-25 23:15:43 -0400

    Revert "Login and register"
    This reverts commit 0bcef6bbddb2c32ef4bebe9671855f35091a3b8c.

--- app/src/main/java/com/example/gymbuddies/RegistrationPage.java
+++ app/src/main/java/com/example/gymbuddies/RegistrationPage.java
@@ -1,58 +1,10 @@
 package com.example.gymbuddies;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.HashMap;
-import java.util.Map;
-
-public class RegistrationPage extends AppCompatActivity {
-    private Button mRegister;
-    private EditText mEmail, mPassword, mName;
-
-    private RadioGroup mRadioGroup;
-
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener firebaseAuthStateListener;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.registration_page);
-
-        mAuth = FirebaseAuth.getInstance();
-        firebaseAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user !=null){
-                    Intent intent = new Intent(RegistrationPage.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                    return;
-                }
-            }
-        };
-
-<<<<<<< Updated upstream
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.ui.AppBarConfiguration;
 
@@ -82,28 +34,33 @@ import java.util.Map;
 
 
 public class RegistrationPage extends AppCompatActivity{
-=======
->>>>>>> Stashed changes
 
-        mRegister = (Button) findViewById(R.id.register);
+        private RegistrationPageBinding binding;
 
-        mEmail = (EditText) findViewById(R.id.email);
-        mPassword = (EditText) findViewById(R.id.password);
-        mName = (EditText) findViewById(R.id.name);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
 
-        mRadioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+            binding = RegistrationPageBinding.inflate(getLayoutInflater());
+            setContentView(R.layout.registration_page);
 
-        mRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                int selectId = mRadioGroup.getCheckedRadioButtonId();
 
-                final RadioButton radioButton = (RadioButton) findViewById(selectId);
+            Button button3;
+            Button button4;
 
-                if(radioButton.getText() == null){
-                    return;
+            button4 = (Button) findViewById(R.id.button4);
+            button3 = (Button) findViewById(R.id.button3);
+            button4.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openNewActivity();
                 }
-<<<<<<< Updated upstream
+            });
+            button3.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openNewActivity();
+                }
             });
 
           //  BottomNavigationView navView = findViewById(R.id.nav_view);
@@ -120,45 +77,10 @@ public class RegistrationPage extends AppCompatActivity{
             Intent intent = new Intent(this, com.example.gymbuddies.MainActivity.class);
             startActivity(intent);
         }
-=======
->>>>>>> Stashed changes
 
-                final String email = mEmail.getText().toString();
-                final String password = mPassword.getText().toString();
-                final String name = mName.getText().toString();
-                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationPage.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if(!task.isSuccessful()){
-                            Toast.makeText(RegistrationPage.this, "sign up error", Toast.LENGTH_SHORT).show();
-                        }else{
-                            String userId = mAuth.getCurrentUser().getUid();
-                            DatabaseReference currentUserDb = FirebaseDatabase.getInstance().getReference().child("Users").child(userId);
-                            Map userInfo = new HashMap<>();
-                            userInfo.put("name", name);
-                            userInfo.put("sex", radioButton.getText().toString());
-                            userInfo.put("profileImageUrl", "default");
-                            currentUserDb.updateChildren(userInfo);
-                        }
-                    }
-                });
-            }
-        });
+
+
     }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(firebaseAuthStateListener);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mAuth.removeAuthStateListener(firebaseAuthStateListener);
-    }
-}
-
 
 
 
